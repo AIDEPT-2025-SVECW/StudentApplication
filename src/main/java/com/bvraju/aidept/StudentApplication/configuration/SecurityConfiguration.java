56235f6fb6edd2ef22f494b94f14856764cc3714
pradeepjuluri143 <pradeep.juluri@gmail.com>
2025-07-29 07:15:42 +0530

    Adding inmemory users authentication

---
+++ src/main/java/com/bvraju/aidept/StudentApplication/configuration/SecurityConfiguration.java
@@ -0,0 +1,32 @@
+package com.bvraju.aidept.StudentApplication.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration {
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        UserDetails user1 = User.withDefaultPasswordEncoder()
+                .username("student")
+                .password("password")
+                .roles("USER")
+                .build();
+
+        UserDetails admin = User.withDefaultPasswordEncoder()
+                .username("admin")
+                .password("admin123")
+                .roles("ADMIN")
+                .build();
+
+        return new InMemoryUserDetailsManager(user1, admin);
+    }
+
+}
